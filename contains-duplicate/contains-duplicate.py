5ff25b27bf15f596f9ce2bc643935ad2588d482a
Subhadip Sarkar <87298461+Subhahdip@users.noreply.github.com>
2022-08-21 21:36:25 +0530

    Time: 659 ms, Memory: 25.9 MB - LeetHub

---
+++ contains-duplicate/contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        buffer = {}
+        
+        for i in range(len(nums)):
+            if nums[i] in buffer:
+                return True
+            buffer[nums[i]] = 1
+            
+        return False
